61616b61558d8e6bc1d148936a16bd8f251dd7f0
Haroldo Leite <holiverleite@gmail.com>
2019-11-09 18:22:42 -0300

    App created in a introduction class about Xamarin.

---
+++ App1Xamarin/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using App1Xamarin.Servico;
+using App1Xamarin.Servico.Modelo;
+
+namespace App1Xamarin
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        public void buscarEndereco(object sender, System.EventArgs args)
+        {
+            String cep = CEP.Text.Trim();
+
+            Endereco end = ViaCEP.buscarEnderecoCEP(cep);
+
+            if (end != null) {
+                MENSAGEM.Text = String.Format("Endereço: {0} - {1}, {2}-{3}", end.logradouro, end.bairro, end.localidade, end.uf);
+            } else {
+                MENSAGEM.Text = "Erro de CEP";
+            }
+        }
+    }
+}
---
+++ App1Xamarin/Servico/ViaCEP.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using App1Xamarin.Servico.Modelo;
+using System.Net;
+using Newtonsoft.Json;
+
+namespace App1Xamarin.Servico
+{
+    public class ViaCEP
+    {
+        private static String enderecoURL = "http://viacep.com.br/ws/{0}/json/";
+
+        public ViaCEP()
+        {
+   
+        }
+
+        public static Endereco buscarEnderecoCEP(String cep) 
+        {
+            String novoEnderecoURL = String.Format(enderecoURL, cep);
+
+            WebClient webClient = new WebClient();
+
+            String conteudo = webClient.DownloadString(novoEnderecoURL);
+
+            if (conteudo == null)
+            {
+                return null;
+            }
+
+            Endereco end = JsonConvert.DeserializeObject<Endereco>(conteudo);
+
+            
+
+            return end;
+        }
+    }
+}
